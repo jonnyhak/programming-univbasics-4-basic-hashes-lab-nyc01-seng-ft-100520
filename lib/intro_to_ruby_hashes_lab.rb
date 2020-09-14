e5b0c54357f90da8ce71072f7b5f67534a7dc8c9
Jonathan Hakimian <jonnyhak@gmail.com>
2020-09-14 14:28:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,10 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  new = {
+    "New York" => "NY",
+    "California" => "CA"
+  }
 end
 
 def pioneer
